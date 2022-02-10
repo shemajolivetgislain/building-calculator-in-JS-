14ad56378bf308d80e509464f0666e34dfa1492b
shemajolivetgislain <yshemajolivet@gmail.com>
2022-02-11 00:00:47 +0200

    condition

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+class Calculator {
+    constructor(previousOperandTextElement, curentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.curentOperandTextElement = curentOperandTextElement
+        this.clear
+    }
+
+    clear(){
+        this.curentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+        this.curentOperand = number
+
+    }
+    chooseOperation(operation){
+
+    }
+    compute(){
+
+    }
+
+    updateDisplay()
+    {
+      this.curentOperandTextElement.innerText = this.curentOperand
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalButtons = document.querySelector('[data-equals]')
@@ -5,3 +38,14 @@ const operationButtons = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-preious-operand]')
 const curentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement,curentOperandTextElement)
+
+numberButtons.forEach(buttons =>{
+    buttons.addEventListener('click', () =>
+    {
+        calculator.appendNumber(buttons.innerText)
+        calculator.updateDisplay()
+    })
+
+})
---
+++ test.js
@@ -0,0 +1,9 @@
+student = ['shema','jolivet', 'jack']
+
+for (let index = 0; index < student.length; index++) {
+    if (student[index].length(4)) {
+        console.log(element) 
+    }
+    
+    
+}
